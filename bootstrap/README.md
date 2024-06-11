393e09e07dcebc8ecb986d86cf61d65a841ee1e7
rbcribeiro <rbcribeiro@gmail.com>
2024-06-11 19:57:49 -0300

    refactor: transferindo tarefa antiga para repositorio geral

---
+++ bootstrap/README.md
@@ -0,0 +1,18 @@
+<h1 align="center">Booststrap</h1>
+
+Conhecendo e aprendendo sobre o desenvolvimento de Carousel e alguns modelos de Cards com o Bootstrap.
+
+<p align="center">
+    <img src=".github/print.png" alt="Imagem do site" width="100%" /></p> 
+    
+<br>
+<h2 align="center"> Desenvolvido por:
+</h2>
+
+<h3 align="center"> Renata Ribeiro 
+
+<br>
+
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/rbcribeiro/)
+[![github](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/rbcribeiro)</h3>
+<br><br>
--- colecoesPart2/src/colecoesPart2/Grupos.java
+++ colecoesPart2/src/colecoesPart2/Grupos.java
@@ -3,46 +3,45 @@ package colecoesPart2;
 import java.util.*;
 
 public class Grupos {
-    public static void main(String[] args) {
-        Scanner leia = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
 
-        List<String> masculino = new ArrayList<>();
-        List<String> feminino = new ArrayList<>();
-        String continuar;
+		List<String> masculino = new ArrayList<>();
+		List<String> feminino = new ArrayList<>();
+		String continuar;
 
-        do {
-            String nomeSexo;
-            String[] partes;
-            String nome, sexo;
+		do {
+			String nomeSexo;
+			String[] partes;
+			String nome, sexo;
 
-            System.out.println("Digite o nome, uma vírgula e o sexo (feminino/masculino): ");
-            nomeSexo = leia.nextLine();
-            partes = nomeSexo.split(",");
-            
-            if (partes.length == 2) {
-                nome = partes[0].trim();
-                sexo = partes[1].trim();
+			System.out.println("Digite o nome, uma vírgula e o sexo (feminino/masculino): ");
+			nomeSexo = leia.nextLine();
+			partes = nomeSexo.split(",");
 
-                if (sexo.equalsIgnoreCase("masculino")) {
-                    masculino.add(nome);
-                } else if (sexo.equalsIgnoreCase("feminino")) {
-                    feminino.add(nome);
-                } else {
-                    System.out.println("Sexo inválido. Use 'feminino' ou 'masculino'.");
-                }
+			if (partes.length == 2) {
+				nome = partes[0].trim();
+				sexo = partes[1].trim();
 
-                System.out.printf("Nome: %s, Sexo: %s%n", nome, sexo);
-            } else {
-                System.out.println("Entrada inválida. Certifique-se de usar uma vírgula para separar o nome e o sexo.");
-            }
+				if (sexo.equalsIgnoreCase("masculino")) {
+					masculino.add(nome);
+				} else if (sexo.equalsIgnoreCase("feminino")) {
+					feminino.add(nome);
+				} else {
+					System.out.println("Sexo inválido. Use 'feminino' ou 'masculino'.");
+				}
 
-            System.out.println("Deseja adicionar outra pessoa? (sim/não)");
-            continuar = leia.nextLine().trim();
-        } while (continuar.equalsIgnoreCase("sim"));
+				System.out.printf("Nome: %s, Sexo: %s%n", nome, sexo);
+			} else {
+				System.out.println("Entrada inválida. Certifique-se de usar uma vírgula para separar o nome e o sexo.");
+			}
 
-        leia.close();
+			System.out.println("Deseja adicionar outra pessoa? (sim/não)");
+			continuar = leia.nextLine().trim();
+		} while (continuar.equalsIgnoreCase("sim"));
 
-        System.out.println("Lista de nomes masculinos: " + masculino);
-        System.out.println("Lista de nomes femininos: " + feminino);
-    }
+		leia.close();
+		System.out.println("Lista de nomes masculinos: " + masculino);
+		System.out.println("Lista de nomes femininos: " + feminino);
+	}
 }
